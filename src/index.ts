047566e818e0912e100881d9c94d925fb4168e22
Keidson Roby <keidsondesigner@gmail.com>
2024-05-20 02:06:54 -0400

    Configuração Deploy Vercel

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+// console.log('DATABASE_URL:', process.env.DATABASE_URL); // Debugging database URL
+
 import { PrismaClient } from "@prisma/client";
 import express from "express";
 
